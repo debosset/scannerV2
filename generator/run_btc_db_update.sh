8a6bea187266d699a96449715221862df61206ba
Adrien de Bosset <debosset@gmail.com>
2025-12-12 18:54:01 +0100

    import db

--- generator/run_btc_db_update.sh
+++ generator/run_btc_db_update.sh
@@ -5,4 +5,4 @@ cd /opt/generator
 exec 9>/tmp/btc_db_update.lock
 flock -n 9 || exit 0
 
-/usr/bin/python3 /opt/generator/btc_db_importer.py --update-daily --test >> /opt/generator/btc_db_importer.log 2>&1
+/usr/bin/python3 /opt/generator/btc_db_importer.py --update-daily --batch-size 5000 --test >> /opt/generator/btc_db_importer.log 2>&1
